2b1c89f890321ec960b657a6e141378b6dd9e408
Nicolas <nicolasmarroyoz@gmail.com>
2025-06-12 13:28:51 -0300

    Ejercicio 1 completado

--- Calculadora/Calculadora.cs
+++ Calculadora/Calculadora.cs
@@ -2,5 +2,42 @@ namespace EspacioCalculadora;
 
 public class Calculadora
 {
+    private double dato = 0;
+
+    public void Sumar(double termino)
+    {
+        dato += termino;
+    }
+
+    public void Restar(double termino)
+    {
+        dato -= termino;
+    }
+
+    public void Multiplicar(double termino)
+    {
+        dato *= termino;
+    }
+
+    public void Dividir(double termino)
+    {
+        if (termino != 0)
+        {
+            dato = dato / termino;
+        }
+        else
+        {
+            Console.WriteLine("Error, no se puede dividir en 0");
+        }
+    }
+
+    public void Limpiar()
+    {
+        dato = 0;
+    }
     
+    public double Resultado
+    {
+        get => dato;
+    }
 }
--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -1,2 +1,74 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using EspacioCalculadora;
+
+void mostrarMenu()
+{
+    Console.WriteLine("\n\n<<<<<< CALCULADORA UNT >>>>>>");
+    Console.WriteLine("1 --> Sumar");
+    Console.WriteLine("2 --> Restar");
+    Console.WriteLine("3 --> Multiplicar");
+    Console.WriteLine("4 --> Dividir");
+    Console.WriteLine("5 --> Limpiar");
+    Console.WriteLine("6 --> Salir");
+    Console.Write("\nSeleccione una opcion: ");
+}
+
+double ingresarTermino()
+{
+    double termino;
+
+    do
+    {
+        Console.Write("Ingrese un numero: ");
+    } while (!double.TryParse(Console.ReadLine(), out termino));
+
+    return termino;
+}
+
+Calculadora calculadora = new Calculadora();
+
+int opcion = 0;
+double termino;
+
+while (opcion != 6)
+{
+    mostrarMenu();
+
+    if (int.TryParse(Console.ReadLine(), out opcion))
+    {
+        switch (opcion)
+        {
+            case 1:
+                termino = ingresarTermino();
+                Console.Write(calculadora.Resultado + " + " + termino + " = ");
+                calculadora.Sumar(termino);
+                Console.WriteLine(calculadora.Resultado);
+                break;
+
+            case 2:
+                termino = ingresarTermino();
+                Console.Write(calculadora.Resultado + " - " + termino + " = ");
+                calculadora.Restar(termino);
+                Console.WriteLine(calculadora.Resultado);
+                break;
+
+            case 3:
+                termino = ingresarTermino();
+                Console.Write(calculadora.Resultado + " * " + termino + " = ");
+                calculadora.Multiplicar(termino);
+                Console.WriteLine(calculadora.Resultado);
+                break;
+
+            case 4:
+                termino = ingresarTermino();
+                Console.Write(calculadora.Resultado + " / " + termino + " = ");
+                calculadora.Dividir(termino);
+                Console.WriteLine(calculadora.Resultado);
+                break;
+
+            case 5:
+                Console.WriteLine("Limpiando la memoria...");
+                calculadora.Limpiar();
+                break;
+        }
+    }
+}
